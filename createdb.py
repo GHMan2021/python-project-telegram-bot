c108707fe3ad83e3f6dbafcfed2fe8c2107f8058
GHMan2021 <statio2009@gmail.com>
2022-01-16 23:24:21 +0400

    fix replace sql on cur

--- createdb.py
+++ createdb.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 db = sqlite3.connect('datebase.db', check_same_thread=False)
-sql = db.cursor()
+cur = db.cursor()
 
-sql.execute("""
+cur.execute("""
     CREATE TABLE IF NOT EXISTS tb_users (
         user_id TEXT,
         user_quality TEXT
@@ -14,39 +14,39 @@ db.commit()
 
 def check_user_id(user_id):
     query = f"SELECT user_id FROM tb_users WHERE user_id = '{user_id}'"
-    sql.execute(query)
-    result = sql.fetchone()
+    cur.execute(query)
+    result = cur.fetchone()
     return result
 
 
 def insert_user_quality(user_id, user_quality):
     query = f"INSERT INTO tb_users VALUES ('{user_id}', '{user_quality}')"
-    sql.execute(query)
+    cur.execute(query)
     db.commit()
 
 
 def update_user_quality(user_id, user_quality):
     query = f'UPDATE tb_users SET user_quality = "{user_quality}" WHERE user_id = "{user_id}"'
-    sql.execute(query)
+    cur.execute(query)
     db.commit()
 
 
 def select_user_quality(user_id):
     query = f"SELECT user_quality FROM tb_users WHERE user_id = '{user_id}'"
-    sql.execute(query)
-    result = sql.fetchone()
+    cur.execute(query)
+    result = cur.fetchone()
     return result[0]
 
 
 def get_user_quality(user_id):
     query = f"SELECT user_quality FROM tb_users WHERE user_id = '{user_id}'"
-    sql.execute(query)
-    result = sql.fetchone()
+    cur.execute(query)
+    result = cur.fetchone()
     return result
 
 
 def get_all():
     query = f"SELECT * FROM tb_users"
-    sql.execute(query)
-    result = sql.fetchall()
+    cur.execute(query)
+    result = cur.fetchall()
     return result
